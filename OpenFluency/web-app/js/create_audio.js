588dabe6c5f7a50220cbda918137a3dc9cf58031
Kim Hamper <kim.hamper@gmail.com>
2014-05-08 09:53:50 -0400

    Add comments/docs for create_audio.js and create_flashcard.js; clean up recorder.js

--- OpenFluency/web-app/js/create_audio.js
+++ OpenFluency/web-app/js/create_audio.js
@@ -1,9 +1,13 @@
+/*----------------------------------------------------------------------------*/
+/* Functionality for Creating Audio Pronunciation Recordings
+/*----------------------------------------------------------------------------*/
+
 var recorder,
 	formData,
 	audio = document.querySelector('audio');
 
 var onFail = function(e){
-	console.log('Rejected!',e);
+	console.log('Rejected!', e);
 };
 
 var onSuccess = function(s){
@@ -14,27 +18,41 @@ var onSuccess = function(s){
 };
 
 window.URL = window.URL || window.webkitURL;
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+navigator.getUserMedia = navigator.getUserMedia ||
+	navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
+	navigator.msGetUserMedia;
 
+/**
+ * Start recording the audio pronunciation file.
+ */
 function startRecording(){
-	console.log("Started Recording");
 	if (navigator.getUserMedia) {onSuccess
 		navigator.getUserMedia({audio: true}, onSuccess, onFail);
-	} else {
-		console.log('navigator.getUserMedia not present');
 	}
 }
 
+/**
+ * End recording of the audio pronunciation file.
+ * @param audioId - the ID of the HTML audio element
+ * @param pronunciationID - the ID of the text pronunciation associated with the
+ * audio file (i.e. selected for the same flashcard).
+ * @param urlStr - the URL where the data will be saved.
+ */
 function stopRecording(audioId, pronunciationID, urlStr){
-	console.log("Stopped Recording");
 	recorder.stop();
 	recorder.exportWAV(function(s){
 		document.querySelector(audioId).src = window.URL.createObjectURL(s);
-		console.log(s);
 		stashBlob(s, pronunciationID, urlStr);
 	});
 }
 
+/**
+ * Create the data packet for the audio file.
+ * @param blob - the audio WAV data.
+ * @param pronunciationID - the ID of the pronunciation the audio is associated
+ * with.
+ * @param urlStr - the URL where the data will be saved.
+ */
 function stashBlob(blob, pronunciationID, urlStr){
 	formData = new FormData();
 	formData.append('blob', blob);
@@ -42,6 +60,9 @@ function stashBlob(blob, pronunciationID, urlStr){
 	formData.append('url', urlStr);
 }
 
+/**
+ * Save the newly recorded audio pronunciation file and associated data.
+ */
 function saveAudio(){
 	// Create the packet
 	var fd = formData;
@@ -54,10 +75,7 @@ function saveAudio(){
 		contentType: false
 	}).done(function(audioInstance) {
 		if(audioInstance.id) {
-			console.log("Success!");
 			window.location = "/OpenFluency/audio/index/";
-		} else {
-			console.log("Something went wrong!");
 		}
 	});
 }
--- OpenFluency/web-app/js/create_flashcard.js
+++ OpenFluency/web-app/js/create_flashcard.js
@@ -1,8 +1,18 @@
+/*----------------------------------------------------------------------------*/
+/* Image/Audio Association Functionality for the Create Flashcard Page
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Flickr Image Search
+/*----------------------------------------------------------------------------*/
+
+// search Flickr for images matching the query
 $("#flickr_search").click(function(){
 	$("#flickr_page_number").val(1).text(1);
 	searchImage("#query", "#results", "#imageLink", 1);
 });
 
+// show next set of results when next button clicked
 $("#flickr_next").click(function(){
 	var targetPage = $("#flickr_page_number").val();
 	targetPage++;
@@ -10,6 +20,7 @@ $("#flickr_next").click(function(){
 	$("#flickr_page_number").val(targetPage).text(targetPage);
 });
 
+// show previous set of results when previous button clicked
 $("#flickr_back").click(function(){
 	var targetPage = $("#flickr_page_number").val();
 	if (targetPage > 1) { 
@@ -19,20 +30,29 @@ $("#flickr_back").click(function(){
 	}
 });
 
-$("#start_rec_button").click(function(){ 
+/*----------------------------------------------------------------------------*/
+/* Record Audio Pronunciation File
+/*----------------------------------------------------------------------------*/
+
+// hide save buttons when recording not in progress
+$("#audioSaveMessage").hide();
+$("#save_rec_button").hide();
+
+// start recording the pronunciation file 
+$("#start_rec_button").click(function(){
 	setWorkerPath("../js/recorderWorker.js");
-	startRecording(); 
+	startRecording();
 	$("#audioSaveMessage").show();
 	$("#goCreate").removeClass('btn-success').addClass('btn-warning');
 });
 
-$("#stop_rec_button").click(function(){ 
-	stopRecording("#audioClip", $("#fc_pronunciation").val(), ""); 
+// stop recording the pronunciation file
+$("#stop_rec_button").click(function(){
+	stopRecording("#audioClip", $("#fc_pronunciation").val(), "");
 	$("#save_rec_button").show();
 });
 
-$("#audioSaveMessage").hide();
-$("#save_rec_button").hide();
+// save the recording and hide the save buttons
 $("#save_rec_button").click(function(){
 	saveAudioRecording(formData);
 	$("#audioSaveMessage").hide();
@@ -40,6 +60,10 @@ $("#save_rec_button").click(function(){
 	$("#goCreate").removeClass('btn-warning').addClass('btn-success');
 });
 
+/**
+ * Save the audio pronunciation file.
+ * @param formDataObj: the audio file and related data to be saved.
+ */
 function saveAudioRecording(formDataObj){
 	// Create the packet
 	var fd = formDataObj;
@@ -52,12 +76,7 @@ function saveAudioRecording(formDataObj){
 		contentType: false
 	}).done(function(audioInstance) {
 		if(audioInstance.id) {
-			console.log("Success!");
 			$("#audio_id").val(audioInstance.id);
-			console.log(audioInstance.id);
-			//window.location = "/OpenFluency/flashcard/save/";
-		} else {
-			console.log("Something went wrong!");
 		}
 	});
 }
