a818be01e261bbf15526795b5271890f845018a1
Matthew Silva <msilva@fas.harvard.edu>
2016-03-06 13:02:33 -0800

    added javadoc comments for Confusers

--- OpenFluency/src/groovy/com/openfluency/confuser/ConfuserFactory.java
+++ OpenFluency/src/groovy/com/openfluency/confuser/ConfuserFactory.java
@@ -2,8 +2,14 @@ package com.openfluency.confuser;
 
 import com.openfluency.language.Language;
 
+/**
+ * Factory class for instantiating instances of the ConfuserInterface
+ */
 public class ConfuserFactory {
 	
+	/**
+	 * Returns true if a confuser implementation exists in the system for the specified language
+	 */
 	public boolean doesConfuserExist(Language language) {
 		
 		if (language.getCode().equals("JAP")) {
@@ -13,6 +19,10 @@ public class ConfuserFactory {
 		return false;
 	}
 	
+	/**
+	 * Returns a ConfuserInterface implementation for the specified language if one exists, returns null otherwise.
+	 * It is recommended to call doesConfuserExist to verify that a confuser exists for the specified language exists before calling this method.
+	 */
 	public ConfuserInterface getConfuser(Language language) {
 		
 		if (language.getCode().equals("JAP")) {
--- OpenFluency/src/groovy/com/openfluency/confuser/JapaneseConfuser.java
+++ OpenFluency/src/groovy/com/openfluency/confuser/JapaneseConfuser.java
@@ -11,6 +11,17 @@ import cscie99.team2.lingolearn.shared.error.ConfuserException;
 
 public class JapaneseConfuser implements ConfuserInterface {
 
+	/**
+	 * Get a random list of confusers of given type limited to the count 
+	 * provided, these results are checked against the black list to 
+	 * ensure that nothing inappropriate is returned. 
+	 * 
+	 * @param word The word (in Japanese) that you want to generate confusers for
+	 * @param alphabet The alphabet (Kanji, Katakana, or Hiragana) that the word is encoded in
+	 * @param count The number that should be returned, if -1 is provided then
+	 * all results are returned without processing.
+	 * @return List of strings containing the confuser results, The length of the list will be between 0 and the requested count.
+	 */
 	@Override
 	public List<String> getConfusers(String word, Alphabet alphabet, int count) throws ConfuserException {
 		
