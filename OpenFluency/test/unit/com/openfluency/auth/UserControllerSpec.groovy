86e6c7276bbcf70475b27c231da286c5e9918ebb
Matthew Silva <msilva@fas.harvard.edu>
2016-03-18 18:25:57 -0400

    fix broken unit tests for UserController

--- OpenFluency/test/unit/com/openfluency/auth/UserControllerSpec.groovy
+++ OpenFluency/test/unit/com/openfluency/auth/UserControllerSpec.groovy
@@ -1,16 +1,15 @@
 package com.openfluency.auth
 
+import com.openfluency.Constants
+import com.openfluency.language.Language;
+import com.sun.media.sound.ModelAbstractChannelMixer;
+
+import grails.plugin.springsecurity.SpringSecurityService
 import spock.lang.*
 
 @TestFor(UserController)
-@Mock(User)
+@Mock([User, Language, Role])
 class UserControllerSpec extends Specification {
-
-    def populateValidParams(params) {
-        assert params != null
-        // TODO: Populate valid properties like...
-        //params["name"] = 'someValidName'
-    }
 
     void "Test the index action returns the correct model"() {
 
@@ -22,40 +21,65 @@ class UserControllerSpec extends Specification {
             model.userInstanceCount == 0
     }
 
-	@Ignore("doesn't work")
     void "Test the create action returns the correct model"() {
+		given: "Parameters specifying the user to create"
+			params.username = "username"
+			params.password = "password"
+			params.enabled = true
+			params.accountExpired = false
+			params.accountLocked = false
+			params.passwordExpired = false
+			params.email = "email@email.com"
+			params.userType = new Role(name: "Student", authority: Constants.ROLE_STUDENT)
+			params.nativeLanguage = new Language(name: 'English', code: 'ENG-US')
+			
+		and: "A mock SpringSecurityService"
+			SpringSecurityService springSecurityService = Stub()
+			springSecurityService.currentUser >> null
+			controller.springSecurityService = springSecurityService
+
         when:"The create action is executed"
-            controller.create()
+            def model = controller.create()
 
         then:"The model is correctly created"
-            model.userInstance!= null
-    }
+            model.userInstance != null
+			model.userInstance.username == "username"
+    }	
 
-	@Ignore("doesn't work")
-    void "Test the save action correctly persists an instance"() {
+	void "Test the save action correctly persists an instance"() {
+		
+		given: "Parameters specifying a valid user instance"
+			def studentRole = new Role(name: "Student", authority: Constants.ROLE_STUDENT).save()
+			def english = new Language(name: 'English', code: 'ENG-US').save()
+			def user = new User(username: "username", password: "password", enabled: true, accountExpired: false, accountLocked: false, passwordExpired: false, email: "email@email.com", userType: studentRole, nativeLanguage: english)			
+			
+			params.username = user.username
+			params.password = "password"
+			params.email = user.email
+			params.userType = [id: String.valueOf(user.userType.id)]
+			params['nativeLanguage.id'] = String.valueOf(user.nativeLanguage.id)
+			params.language = []
+			params.proficiency = []
 
-        when:"The save action is executed with an invalid instance"
-            request.contentType = FORM_CONTENT_TYPE
-            def user = new User()
-            user.validate()
-            controller.save(user)
+		and: "A mock SpringSecurityService"
+			SpringSecurityService springSecurityService = Stub()
+			springSecurityService.currentUser >> null
+			controller.springSecurityService = springSecurityService
+			
+		and: "A mock UserService"
+			UserService userService = Stub() {
+				createUser(_) >> { return user.save(flush: true) }
+			}
+			
+			controller.userService = userService
+			
+		when: "The save action is executed"
+			controller.save()
 
-        then:"The create view is rendered again with the correct model"
-            model.userInstance!= null
-            view == 'create'
-
-        when:"The save action is executed with a valid instance"
-            response.reset()
-            populateValidParams(params)
-            user = new User(params)
-
-            controller.save(user)
-
-        then:"A redirect is issued to the show action"
-            response.redirectedUrl == '/user/show/1'
-            controller.flash.message != null
-            User.count() == 1
-    }
+		then:"A redirect is issued to the home page"
+			response.redirectedUrl == '/'
+			controller.flash.message != null		
+	}
 
     void "Test that the show action returns the correct model"() {
         when:"The show action is executed with a null domain"
@@ -65,7 +89,6 @@ class UserControllerSpec extends Specification {
             response.status == 404
 
         when:"A domain instance is passed to the show action"
-            populateValidParams(params)
             def user = new User(params)
             controller.show(user)
 
@@ -73,79 +96,90 @@ class UserControllerSpec extends Specification {
             model.userInstance == user
     }
 
-	@Ignore("doesn't work")
     void "Test that the edit action returns the correct model"() {
-        when:"The edit action is executed with a null domain"
-            controller.edit(null)
 
-        then:"A 404 error is returned"
-            response.status == 404
-
-        when:"A domain instance is passed to the edit action"
-            populateValidParams(params)
+		given: "A mock SpringSecurityService"
+			SpringSecurityService springSecurityService = Stub()
+			controller.springSecurityService = springSecurityService
+			
+        and: "A domain instance"
+			params.username = "username"
+			params.password = "password"
+			params.enabled = true
+			params.accountExpired = false
+			params.accountLocked = false
+			params.passwordExpired = false
+			params.email = "email@email.com"
+			params.userType = new Role(name: "Student", authority: Constants.ROLE_STUDENT)
+			params.nativeLanguage = new Language(name: 'English', code: 'ENG-US')
             def user = new User(params)
-            controller.edit(user)
+			user.springSecurityService = springSecurityService
+			user.save()
+			
+			springSecurityService.principal >> user
+		
+		and: "HTTP method is POST"
+			request.method = 'POST'
+			
+		when: "The edit action is executed"
+			def model = controller.edit()
 
-        then:"A model is populated containing the domain instance"
+        then: "A model is populated containing the domain instance"
             model.userInstance == user
     }
 	
-	@Ignore("doesn't work")
     void "Test the update action performs an update on a valid domain instance"() {
-        when:"Update is called for a domain instance that doesn't exist"
-            request.contentType = FORM_CONTENT_TYPE
-            controller.update(null)
-
-        then:"A 404 error is returned"
-            response.redirectedUrl == '/user/index'
-            flash.message != null
-
-
-        when:"An invalid domain instance is passed to the update action"
-            response.reset()
-            def user = new User()
-            user.validate()
-            controller.update(user)
-
-        then:"The edit view is rendered again with the invalid instance"
-            view == 'edit'
-            model.userInstance == user
-
+		given: "Parameters specifying the user to create"
+			params.username = "username"
+			params.password = "password"
+			params.enabled = true
+			params.accountExpired = false
+			params.accountLocked = false
+			params.passwordExpired = false
+			params.email = "email@email.com"
+			params.userType = new Role(name: "Student", authority: Constants.ROLE_STUDENT)
+			params.nativeLanguage = new Language(name: 'English', code: 'ENG-US')
+			
+		and: "A mock SpringSecurityService"
+			SpringSecurityService springSecurityService = Stub()
+			controller.springSecurityService = springSecurityService
+			
+		and: "A mock UserService"
+			UserService userService = Stub()	
+			controller.userService = userService
+		
         when:"A valid domain instance is passed to the update action"
-            response.reset()
-            populateValidParams(params)
-            user = new User(params).save(flush: true)
+            User user = new User(params)
+			user.springSecurityService = springSecurityService
+			user.save(flush: true)
             controller.update(user)
 
-        then:"A redirect is issues to the show action"
-            response.redirectedUrl == "/user/show/$user.id"
-            flash.message != null
+        then:"A redirect is issued to the home page"
+            response.redirectedUrl == "/"
+            flash.message == "${user.username}, your profile has been updated."
     }
 	
-	@Ignore("doesn't work")
-    void "Test that the delete action deletes an instance if it exists"() {
-        when:"The delete action is called for a null instance"
-            request.contentType = FORM_CONTENT_TYPE
-            controller.delete(null)
-
-        then:"A 404 is returned"
-            response.redirectedUrl == '/user/index'
-            flash.message != null
-
-        when:"A domain instance is created"
-            response.reset()
-            populateValidParams(params)
-            def user = new User(params).save(flush: true)
-
-        then:"It exists"
-            User.count() == 1
-
-        when:"The domain instance is passed to the delete action"
+	void "Test that the delete action deletes an instance if it exists"() {
+		
+		given:"A user"
+			def studentRole = new Role(name: "Student", authority: Constants.ROLE_STUDENT).save()
+			def english = new Language(name: 'English', code: 'ENG-US').save()
+			def user = new User(username: "username", password: "password", enabled: true, accountExpired: false, accountLocked: false, passwordExpired: false, email: "email@email.com", userType: studentRole, nativeLanguage: english)
+		
+		and: "A mock SpringSecurityService"
+			SpringSecurityService springSecurityService = Stub()
+			user.springSecurityService = springSecurityService
+			
+		when: "The save method is called on the user object"
+			user.save()
+			
+		then: "The user is saved"
+			User.count() == 1
+				
+        when:"The user is passed to the delete action"
             controller.delete(user)
 
         then:"The instance is deleted"
             User.count() == 0
-            response.redirectedUrl == '/user/index'
-            flash.message != null
     }
 }
